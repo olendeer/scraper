e1de890de4c7dbdcbddbf6c1ef750d71e0e94f61
olendeer <olendeer666@gmail.com>
2020-05-06 10:03:04 +0300

    new release

--- server.js
+++ server.js
@@ -62,23 +62,23 @@ cron.schedule('*/1 * * * *', async () => {
 			})
 		})
 		
-	// let startScrape = setTimeout(() => {
-	// // 	console.log('Start scraper live events')
-	// 	filtersActive.forEach(filter => {
-	// 		EventLineItem.find({live: true, sport: filter.sport, fora: {$gte: filter.fora[0], $lte: filter.fora[1]}, total: {$gte: filter.total[0], $lte: filter.total[1]}, coefficient: {$gte: filter.difference[0], $lte: filter.difference[1]}})
-	// 		.then(events => {
-	// 				scrapSportLive(events, filter)
-	// 				.then(result => {
-	// 					// console.log(result)
-	// 				})
-	// 		})
-	// 	})
-	// // // // 	// countScraps ++;
-	// // // // 	// if(countScraps == 5){
-	// // // // 	// 	console.log('Cron reload');
-	// // // // 	// 	clearInterval(startScrape)
-	// // // // 	// }
-	// }, 15000);
+	let startScrape = setTimeout(() => {
+	// 	console.log('Start scraper live events')
+		filtersActive.forEach(filter => {
+			EventLineItem.find({live: true, sport: filter.sport, fora: {$gte: filter.fora[0], $lte: filter.fora[1]}, total: {$gte: filter.total[0], $lte: filter.total[1]}, coefficient: {$gte: filter.difference[0], $lte: filter.difference[1]}})
+			.then(events => {
+					scrapSportLive(events, filter)
+					.then(result => {
+						// console.log(result)
+					})
+			})
+		})
+	// // // 	// countScraps ++;
+	// // // 	// if(countScraps == 5){
+	// // // 	// 	console.log('Cron reload');
+	// // // 	// 	clearInterval(startScrape)
+	// // // 	// }
+	}, 15000);
 });
 
 
@@ -684,7 +684,7 @@ app.use(express.static('public'));
 
 
 async function scrapSportLive(events, filter){
-	const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox'], ignoreDefaultArgs: ['--disable-extensions']});
+	const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox'], ignoreDefaultArgs: ['--disable-extensions']});
 	let page = await browser.newPage();
 	process.on('unhandledRejection', (reason, p) => {
 	    // console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
